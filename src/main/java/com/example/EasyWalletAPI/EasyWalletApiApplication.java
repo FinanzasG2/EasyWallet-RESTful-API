e79946e7d9969a905e3a4181051dc66e9990c28b
William062004 <U202117904@upc.edu.pe>
2024-09-25 01:07:27 -0500

    Inicio de Backend

---
+++ src/main/java/com/example/EasyWalletAPI/EasyWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EasyWalletAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EasyWalletAPI/EasyWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EasyWalletAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

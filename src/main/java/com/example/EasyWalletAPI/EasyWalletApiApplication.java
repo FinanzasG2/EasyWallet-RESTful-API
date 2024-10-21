884afe5c5519079dbdbc377b4d3a58c0b1374703
William062004 <U202117904@upc.edu.pe>
2024-10-21 04:02:03 -0500

    Security Update

--- src/main/java/com/example/EasyWalletAPI/EasyWalletApiApplication.java
+++ src/main/java/com/example/EasyWalletAPI/EasyWalletApiApplication.java
@@ -3,9 +3,15 @@ package com.example.EasyWalletAPI;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@OpenAPIDefinition(info = @Info(title = "EasyWallet API", version = "v1"))
+@OpenAPIDefinition(
+		info = @Info(title = "EasyWallet API", version = "v1"),
+		servers = {
+				@Server(url = "https://easywallet-restful-api.up.railway.app", description = "Railway API Server")
+		}
+)
 @SpringBootApplication
 public class EasyWalletApiApplication {
 
